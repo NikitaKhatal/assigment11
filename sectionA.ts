bae2614658ccba99e50e3c30c8b562bc1464c23e
NikitaKhatal <100146293+NikitaKhatal@users.noreply.github.com>
2022-05-09 13:53:21 +0530

    typescript

---
+++ sectionA.ts
@@ -0,0 +1,41 @@
+//QA1
+console.log("The King said\"Begin at the beginning!\"to me");
+//QA2
+console.log("BEST");
+console.log("OF LUCK");
+//QA3
+var y:number=8;
+y+=++y+y--+--y;
+console.log(y);
+//QA4
+var a:number=7;
+a+=a++ + ++a + --a +a--;
+console.log(a);
+//QA5
+var y1:number=10;
+var z1:number=(++y1 *(y1++ +5));
+console.log(z1);
+//QA6
+var a1:number=63;
+var b1:number=36;
+var x1:boolean=(a1<b1)?true:false;
+var y2:number=(a1>b1)?a1:b1;
+console.log(x1);
+console.log(y2);
+//QA7
+var x2:number=4;
+x2+=(x2++)+(++x2)+x2;
+console.log(x2);
+//QA8
+var k:number=5;
+var j:number=9;
+k+=k++ - ++j +k;
+console.log("k="+k);
+console.log("j="+j);
+//QA9
+console.log("Incredible" + "\n" +"World");
+//QA10.
+// i.< =less than
+// ii.++ =increment
+// iii.&& =logical and
+// iv.?:=ternary operator
